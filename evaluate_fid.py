47eec9d50467ccf0aef87afb9a4d8848213ff3ca
mjy8086 <mjy8086@korea.ac.kr>
2021-12-03 18:08:48 +0900

    update

---
+++ evaluate_fid.py
@@ -0,0 +1,109 @@
+import os
+import matplotlib.pyplot as plt
+import argparse
+import torch
+from torch import nn
+from torch import Tensor
+from torch.nn.modules.module import Module
+import torch.nn.functional as F
+from torch.nn import Dropout, Softmax, Linear, Conv3d, LayerNorm, Flatten, Conv2d
+import torchvision
+from torchvision import transforms, models
+import math
+import copy
+from torch import autograd
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import numpy as np
+from tqdm import tqdm
+from itertools import cycle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import cv2
+from scipy.ndimage.filters import gaussian_filter
+from scipy.stats import norm
+from pytorch_fid.inception import InceptionV3
+
+
+from tqdm import tqdm
+from model_test import *
+from utility import *
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+torch.cuda.empty_cache()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--batch_size', type=int, default=100)
+args = parser.parse_args()
+
+model = InceptionV3().cuda()
+
+
+CelebA_Encoder_path = "/home/mjy/AvatarGAN/generated_img/epoch_100/CelebA_Encoder.pth"
+Bottleneck_path = "/home/mjy/AvatarGAN/generated_img/epoch_100/Bottleneck.pth"
+Cartoon_Decoder_path = "/home/mjy/AvatarGAN/generated_img/epoch_100/Cartoon_Decoder.pth"
+
+
+
+CelebA_Encoder = CelebA_Encoder().cuda()
+CelebA_Encoder.load_state_dict(torch.load(CelebA_Encoder_path))
+CelebA_Encoder.cuda()
+
+Bottleneck = Bottleneck().cuda()
+Bottleneck.load_state_dict(torch.load(Bottleneck_path))
+Bottleneck.cuda()
+
+Cartoon_Decoder = Cartoon_Decoder().cuda()
+Cartoon_Decoder.load_state_dict(torch.load(Cartoon_Decoder_path))
+Cartoon_Decoder.cuda()
+
+
+for param in CelebA_Encoder.parameters():
+    param.requires_grad_(False)
+for param in Bottleneck.parameters():
+    param.requires_grad_(False)
+for param in Cartoon_Decoder.parameters():
+    param.requires_grad_(False)
+
+
+
+
+def main():
+    batch_size = args.batch_size
+
+    test_CelebA_loader = DataLoader(test_CelebA, batch_size=batch_size, shuffle=False, drop_last=True)
+
+    CelebA_Encoder.eval()
+    Bottleneck.eval()
+    Cartoon_Decoder.eval()
+
+
+    FID_score = 0
+
+
+    with torch.no_grad():
+
+        for batch, test_data in tqdm(enumerate(test_CelebA_loader), total=len(test_CelebA_loader)):
+            test_img = test_data
+
+            test_img = Variable(test_img).cuda()
+
+
+            e_out, c5, c4, c3, c2, c1 = CelebA_Encoder(test_img)
+            b_out = Bottleneck(e_out)
+            fake_img = Cartoon_Decoder(b_out, c5, c4, c3, c2, c1)
+
+            fid = calculate_fretchet(test_img, fake_img.cuda(), model, device='cuda')
+
+            FID_score += fid.item()
+
+    print('Cartoon_D_loss: {:3f}'.format(FID_score / len(test_CelebA_loader)))
+
+
+
+
+if __name__ == '__main__':
+    main()
--- utility.py
+++ utility.py
@@ -8,7 +8,9 @@ from PIL import Image
 import cv2
 import albumentations as A
 import os
-
+from scipy import linalg
+from torch.nn.functional import adaptive_avg_pool2d
+from pytorch_fid.inception import InceptionV3
 
 
 # set image path
@@ -97,8 +99,85 @@ CelebA_data_target = CelebA_Dataset(CelebA_path, CelebA, Resize, mean, std)
 
 test_CelebA = CelebA_Dataset(test_CelebA_path, test_CelebA, Resize, mean, std)
 
-# dataloader
-# batch_size = 32
 
-# Cartoon_loader = DataLoader(Cartoon_data, batch_size=batch_size, shuffle=True, drop_last=True)
-# CelebA_loader = DataLoader(CelebA_data, batch_size=batch_size, shuffle=True, drop_last=True)
+
+
+"""
+FID score
+"""
+
+
+def calculate_activation_statistics(images, model, device=None, batch_size=128, dims=2048):
+    model.eval()
+    act = np.empty((len(images), dims))
+
+    if device is None:
+        batch = images
+    else:
+        batch = images.to(device)
+    pred = model(batch)[0]
+
+    # If model output is not scalar, apply global spatial average pooling.
+    # This happens if you choose a dimensionality not equal 2048.
+    if pred.size(2) != 1 or pred.size(3) != 1:
+        pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+    act = pred.cpu().data.numpy().reshape(pred.size(0), -1)
+
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
+
+
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) +
+            np.trace(sigma2) - 2 * tr_covmean)
+
+
+
+
+
+def calculate_fretchet(images_real, images_fake, model, device):
+    mu_1, std_1 = calculate_activation_statistics(images_real, model, device=device)
+    mu_2, std_2 = calculate_activation_statistics(images_fake, model, device=device)
+
+    """get fretched distance"""
+    fid_value = calculate_frechet_distance(mu_1, std_1, mu_2, std_2)
+    return fid_value
